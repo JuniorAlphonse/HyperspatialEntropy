771d72f78e1b6e8b01b1910fd0ae1f297862c346
Junior Alphonse <97910760+JuniorAlphonse@users.noreply.github.com>
2024-09-05 21:59:41 -0400

    Add files via upload
    These are the completion of the version 1.5 of Hyperspatial Entropy projects as well as a file of the aforementioned Temporal Bits portion.

---
+++ HSEREADME.md
@@ -0,0 +1,19 @@
+# HyperspatialEntropy&InformationCommunication
+This project will host the code for natural physical codes pertaining to the advance of Science and The Scientific Method (hopefully).
+
+## Code's Purpose
+This project will attempt to verify whether or not information transferrence is limited to the observable universe or whether or not our universe is alone. This loneliness is dependent on the presence of a hyperspatial vaccum and0 or other universes in said unknown spaces. It will also attempts to unify Boltzmann's Entropy with Shannon's Informative Entropy. This code may rely on an introduction to Information Entropy found at the following link: https://machinelearningmastery.com/what-is-information-entropy/. Thanks to said introduction, astrophysicists, cosmologists, mathematicians, electrical, mechanical, and aerospace engineers may determine the feasibility of interspatial travel through presupposed warp drives or elsewise. Additional guidance was sought from a paper by the University of Houston, which can be found at the following link: https://www.math.uh.edu/~etgen/ComplexNos.pdf. The following definition of real numbers was also applicable: https://www.britannica.com/science/real-number.
+
+### Technologies
+1. Python (Visual Studio Code (VS Code) or Anaconda)
+2. Matplotlib & pyplot
+3. (pending Ethical Quantum AI!)
+
+#### Installation and Usage Instructions
+This program requires the latest editions of VS Code or the equivalent in Anaconda's Jupyter Notebook services. This code is highly experimentially and would not qualify for a version dot 1.0 yet but may soon be so given time. For now, it is best used for theoretical postulations and materials testing for pending interstellar Missions. The eventual is to create a system interface for a warp drive space shuttle or spacecraft or a fleet of such.
+
+##### Unveiling
+Proper citation and stylization is MANDATORY for all works, deritative of this particular project and all its iterations!
+
+###### Condolences and (&) Dedications
+I humbly thank all of sources that inspired this work; thank you for helping to find my fufilllment{s}.
---
+++ hyperspatial_entropy.py
@@ -0,0 +1,44 @@
+# Exploration of Entropy in Hyperspatial Vaccum(s)
+
+# Q1: Is The Second Law of Thermodyanmics is mandatory for "superpentaspaces" (such that dimensionalities are a total of five or more spacetime usually with 3 or more spatial dimensions, with at least one, but preferably, three or more time dimensions) or do different laws apply to manifolds and therefore spatial dimension cappers, as well as temporal dimensional cappers?
+import math
+
+#Q2: Is pi, the possibility of safety in measuring the usage of the sun to determine time left before the natural logarithm of of the danger of the night sky of the enemy of the Homo Sapiens Sapiens as the Ancient philosophers Archimedes, Euclid, Socrates, Plato, and Ada Lovelace.
+from math import pi
+from math import PI
+# The idea to import all of math may be crucial to the viability of this program and all subsequent programs!
+from  math import log2
+from matplotlib import pyplot
+
+# Step 1: List all of the probabilities of "microstates" or possible outcomes to be more general
+# Note about the definition of the period is, as per Junior E. Alphonse AND(and) Nina Alphonse 
+
+probability_list = [1.95601150271, 1.0, 2.0, 3.0]
+
+# Step 2: Define Information with base 2 modulation to accomodate bits
+
+information = [log2 for p in probability_list]
+
+def Information():
+    for lst in Information:
+        return lst;
+        for p in lst:
+            return p;
+
+    print("Information List and Probability per Item List: " + str(Information)); 
+
+# Step 2.5: Print the information list list comprehension variable.
+ 
+print(information);
+
+# Step 3: Define Information with the natural log based modulation to accomodate "nats" or naturual bits.
+
+entropy = len(2*int(pi)*probability_list)
+svaArrheniusBoltamannDefofEntropy = ((2.9979*10*8)*(1.38*10^-23)*4)
+
+# Q(?): What is the graph of the probability list, information, and entropy?
+
+print(pyplot(probability_list, information, Information(probability_list)));
+
+# Step 4: Define the graph of the probability list, information, entropy, and whatever may come next as, maybe instantaneous or even faster than light travel through Cerchanovic Radiation or the speed of a shadow as defined by Michael of VSauce.
+
