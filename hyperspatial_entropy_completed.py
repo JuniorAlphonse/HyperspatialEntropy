c61e14d5ae5a8093aa410888dbdfd1ab6d4164f2
Junior Alphonse <97910760+JuniorAlphonse@users.noreply.github.com>
2024-10-09 03:02:45 -0400

    Update hyperspatial_entropy_completed.py
    git commit -m "Complete the probability versus information graphing code"

--- hyperspatial_entropy_completed.py
+++ hyperspatial_entropy_completed.py
@@ -1,31 +1,34 @@
 # Exploration of Entropy in Hyperspatial Vaccum(s)
 
 # Q1: Is The Second Law of Thermodyanmics is mandatory for "superpentaspaces" (such that dimensionalities are a total of five or more spacetime usually with 3 or more spatial dimensions, with at least one, but preferably, three or more time dimensions) or do different laws apply to manifolds and therefore spatial dimension cappers, as well as temporal dimensional cappers?
+
 import math
 
 #Q2: Is pi, the possibility of safety in measuring the usage of the sun to determine time left before the natural logarithm of of the danger of the night sky of the enemy of the Homo Sapiens Sapiens as the Ancient philosophers Archimedes, Euclid, Socrates, Plato, and Ada Lovelace.
 from math import pi
-from math import PI
 # The idea to import all of math may be crucial to the viability of this program and all subsequent programs!
-from  math import log2
+from math import log2
 from matplotlib import pyplot
 
 # Step 1: List all of the probabilities of "microstates" or possible outcomes to be more general
-# Note about the definition of the period is, as per Junior E. Alphonse AND(and) Nina Alphonse 
+# Note about the definition of the period is, as per Junior E. Alphonse AND(and) Nina DeFrancisco Alphonse 
+# The purpose of the following proability_list to display the likelihood of a few given configurations for Information Theory. These few have far reaching meaninings. The first microstate given below defines the probability of encountering spatial, as from spacetime, information that can be observed by measurement. The second probability represents the probability of measuring both spatial and temporal phenomena and or epiphenomena, such as time travel as one or one's vehicle approaches a sizeable portion of the speed of light. The third probability displays the certainty of the dimensions four through nine, or ten if M-theory is found to be observable. A probability of higher than 1.0 verifies that all dimensions, of spatial, temporal, and compact dimensions are probabilistically certain. 
+# Knowing that a microstate information of 1.0 means that an outcome is certain and therefore required to occur. Then summing the probabilities of the spatial, temporal, and compact dimensions and having essentially 300% certainty verifies that all proposed dimensionalities must exist or the graph would not be not possible on a classiscal computer, not to mention a quantum computer.
 
-probability_list = [1.95601150271, 1.0, 2.0, 3.0]
+probability_list = [1.00, 2.00, 3.00]
 
-# Step 2: Define Information with base 2 modulation to accomodate bits
+# Note that despite the fact that probability typically has a max value of 1.00, or 100%, values of two and three are displayed. These values are relatively to the definition of superposition as modified by Junior Alphonse from a classical mechanical reimagining of the Einsteinian special relativity. This reimagining allows for the
 
-information = [log2 for p in probability_list]
+# Step 2: Define the information variable by assigning a logarithmic to act upon the probability_list vector using list comprhension.
+information = [-log2(p) for p in probability_list]
 
-def Information():
-    for lst in Information:
-        return lst;
-        for p in lst:
-            return p;
+# def Information(lst):
+   # for lst in Information:
+    #    return lst;
+     #   for p in lst:
+      #      return p;
 
-    print("Information List and Probability per Item List: " + str(Information)); 
+    #print("Information List and Probability per Item List: " + str(Information)); 
 
 # Step 2.5: Print the information list list comprehension variable.
  
@@ -33,12 +36,23 @@ print(information);
 
 # Step 3: Define Information with the natural log based modulation to accomodate "nats" or naturual bits.
 
-entropy = len(2*int(pi)*probability_list)
-svaArrheniusBoltamannDefofEntropy = ((2.9979*10*8)*(1.38*10^-23)*4)
+entropy = len(2*int(pi)*probability_list);
+svaArrheniusBoltzamannDefofEntropy = ((2.9979 * 10**8)*(1.38 * 10**-23)*4);
+print(entropy);
+print(svaArrheniusBoltzamannDefofEntropy);
 
 # Q(?): What is the graph of the probability list, information, and entropy?
 
-print(pyplot(probability_list, information, Information(probability_list)));
+pyplot.plot(probability_list, information);
 
 # Step 4: Define the graph of the probability list, information, entropy, and whatever may come next as, maybe instantaneous or even faster than light travel through Cerchanovic Radiation or the speed of a shadow as defined by Michael of VSauce.
 
+pyplot.title('Probability vs information');
+pyplot.xlabel('Probability');
+pyplot.ylabel('information');
+pyplot.show();
+
+# Note that the probability vs. information curve has an offset for the 1.9 probability displaying the probability of a near-hit possibility that is just short of actually happening.
+
+# Secret Service, FBI, Pentagon, All Military Police, Rangers, Spec-Ops: Ranger Elite Team(s), RTOF, and STEMists (R&D), CallOut and SendOff Drone and BlueJays Squadron and Airforce RMs': LiftOff GodSpeed, WarpForce:
+# helicalDNA = [hsincos(x*y*2, z*3, a*x*y+b*y^z^2+c*a*x^2*y*z^2, c*a*x*2*y*((2/(3/2))*(pi*e^(i^pi^2)+z^16/9)), D^(16*(1000)))];
